a412d852f3c4cee01346107aadf22c32e3680e05
Aniket Rao <aniketamazon54@gmail.com>
2017-08-31 17:18:50 -0700

    Adding component states and doing a GET on albums API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+
+state = { albums: [] }; // set initial state of component
+
+componentWillMount() {
+  /*
+    update state of component once get request is complete
+    and render the component once again.
+  */
+  fetch('https://rallycoding.herokuapp.com/api/music_albums')
+  .then((response) => response.json())
+  .then((responseData) => this.setState({ albums: responseData }));
+}
 render() {
+  console.log(this.state);
   return (
     <View>
       <Text>Album List! </Text>
