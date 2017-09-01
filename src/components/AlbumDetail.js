f14252c47a9e57cda5b61472fda56d01d6ba98b1
Aniket Rao <aniketamazon54@gmail.com>
2017-09-01 13:34:57 -0700

    Added major styling to card

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
   return (
-  <View>
-    <Text>
-      {props.album.title}
-    </Text>
-  </View>
+  <Card>
+    <CardSection>
+    <View style={styles.firstCardSectionHeader}>
+      <Image
+       style={styles.thumbnailStyle}
+       source={{ uri: props.album.thumbnail_image }}
+      />
+    </View>
+    <View style={styles.firstCardSection}>
+      <Text style={styles.titleStyle}>{props.album.title}</Text>
+      <Text>{props.album.artist}</Text>
+    </View>
+    </CardSection>
+
+    <CardSection>
+      <Image
+      style={styles.imageStyle}
+      source={{ uri: props.album.image }}
+      />
+    </CardSection>
+  </Card>
   );
+};
+
+const styles = {
+  firstCardSection: {
+    justifyContent: 'space-around',
+    flexDirection: 'column'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  firstCardSectionHeader: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+    marginRight: 15
+  },
+  titleStyle: {
+    fontSize: 18
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
