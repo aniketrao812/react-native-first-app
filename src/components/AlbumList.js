c3298cda6afee07ae5f9ca9d2bbe6323c71a7ac2
Aniket Rao <aniketamazon54@gmail.com>
2017-08-31 18:14:42 -0700

    Added AlbumDetail Component, passed album as prop in order to display in AlbumDetail Component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
@@ -16,7 +17,9 @@ componentWillMount() {
 }
 
 renderAlbums() {
-  return this.state.albums.map((album) => <Text>{album.title}</Text>);
+  return this.state.albums.map((album) =>
+   <AlbumDetail key={album.title} album={album} />
+ );
 }
 
 render() {
